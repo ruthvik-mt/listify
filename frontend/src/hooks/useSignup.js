5c56b981cbbf2a31c776fdbfdc1a1cfab31b6e6b
Ruthvik101 <ruthvikmt001@gmail.com>
2025-09-21 22:24:28 +0530

    routes

--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -11,7 +11,7 @@ export const useSignup = () => {
     setIsLoading(true)
     setError(null)
 
-    const response = await fetch(`${API_URL}/api/user/login`, {
+    const response = await fetch(`${API_URL}/api/user/signup`, {
       method: 'POST',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({ email, password })
